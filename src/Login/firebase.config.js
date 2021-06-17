882d01d98a0eacde355144a0e2554e751739275e
rafid365.info@gmail.com <rafid365.info@gmail.com>
2021-06-17 16:07:27 +0600

    Reservte yu r Ride

---
+++ src/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0pOgyHX71PJ2URKgn-JAgAXcjCSk-SHQ",
+    authDomain: "ride-hunt.firebaseapp.com",
+    projectId: "ride-hunt",
+    storageBucket: "ride-hunt.appspot.com",
+    messagingSenderId: "936853884884",
+    appId: "1:936853884884:web:af760039e8d2f730a99041"
+  };
+export default firebaseConfig;
